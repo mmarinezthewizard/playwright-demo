a22d2af4c26b32b16596986a810b374828f39409
Manuel Marinez <mmarinez@syneteksolutions.com>
2023-05-07 08:17:36 -0400

    Remove headed configuration from webdriver

--- webdriver/Chrome.py
+++ webdriver/Chrome.py
@@ -4,7 +4,7 @@ from playwright.sync_api import sync_playwright
 class ChromeBrowser:
    # with sync_playwright() as webdriver:
     webdriver = sync_playwright().start()
-    browser = webdriver.chromium.launch(channel="chrome", headless=False)
+    browser = webdriver.chromium.launch(channel="chrome")
     page = browser.new_page()
         
     
